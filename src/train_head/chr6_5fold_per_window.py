17c7e524a56be253f043d8942c8844b6e3b9c301
Hussin El Rashidy <56107521+huRashidy@users.noreply.github.com>
2025-11-05 13:34:53 +0200

    Update src/README.md and src/train_head/chr6_5fold_per_window.py per user changes

--- src/train_head/chr6_5fold_per_window.py
+++ src/train_head/chr6_5fold_per_window.py
@@ -89,12 +89,12 @@ def predict_by_window(embeddings_all, y, method="WLR"):
 
 def main_refined_windows(chromosome, model_name):
     if chromosome == 21:
-        with open(f"/data/horse/ws/huel099f-ancestry_task/data/chr21/chr21_labels_All.pkl", "rb") as f:
+        with open(f"./data/chr21/chr21_labels_All.pkl", "rb") as f:
             labels_all = pickle.load(f)
     elif chromosome == 6:
-        with open("/data/horse/ws/huel099f-ancestry_task/data/chr6/chr6_labels_HLA_252.pkl", "rb") as f:
+        with open("./data/chr6/chr6_labels_HLA_252.pkl", "rb") as f:
             labels_252 = pickle.load(f)
-        with open("/data/horse/ws/huel099f-ancestry_task/data/chr6/chr6_labels_HLA_2296.pkl", "rb") as f:
+        with open("./data/chr6/chr6_labels_HLA_2296.pkl", "rb") as f:
             labels_2296 = pickle.load(f)
         labels_all = np.concatenate((labels_252, labels_2296), axis=0)
     y = np.squeeze(labels_all, axis=1) if labels_all.ndim > 1 else labels_all
@@ -104,7 +104,7 @@ def main_refined_windows(chromosome, model_name):
         print(f"Loading {emb} embeddings for chromosome {chromosome}")
         if chromosome == 21:
             print("Processing chromosome 21")
-            with open(f"/data/horse/ws/huel099f-ancestry_task/data/chr21/HyenaDNA450k_{emb}_chr21_concat/chr21_{emb}_embeddings.pkl", "rb") as f:
+            with open(f"./data/chr21/HyenaDNA450k_{emb}_chr21_concat/chr21_{emb}_embeddings.pkl", "rb") as f:
                 ind_all = pickle.load(f)
             sys.stdout.flush()
 
@@ -113,26 +113,25 @@ def main_refined_windows(chromosome, model_name):
             print("Processing chromosome 6")
             sys.stdout.flush()
 
-            with open(f"/data/horse/ws/huel099f-ancestry_task/data/chr6/HyenaDNA450k_{emb}_252/chr6_{emb}_embeddings.pkl", "rb") as f:
+            with open(f"./data/chr6/HyenaDNA450k_{emb}_252/chr6_{emb}_embeddings.pkl", "rb") as f:
                 all_252 = pickle.load(f)
 
-            with open(f"/data/horse/ws/huel099f-ancestry_task/data/chr6/HyenaDNA450k_{emb}_2296_concat/chr6_{emb}_embeddings.pkl", "rb") as f:
+            with open(f"./data/chr6/HyenaDNA450k_{emb}_2296_concat/chr6_{emb}_embeddings.pkl", "rb") as f:
                 all_2296 = pickle.load(f)
             ind_all = np.concatenate((all_252, all_2296), axis=0)
-
 
 
         print(f"Embeddings shape: {ind_all.shape}")
         sys.stdout.flush()
 
         f1_scores = predict_by_window(ind_all, y, method=model_name)
-        np.save(f"/data/horse/ws/huel099f-ancestry_task/results/f1_scores_{model_name}_{emb}_chr{chromosome}_windows.npy", f1_scores)
+        np.save(f"./data/results/f1_scores_{model_name}_{emb}_chr{chromosome}_windows.npy", f1_scores)
         del ind_all, f1_scores
         gc.collect()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run per-window training with low memory usage")
-    parser.add_argument('--chr', type=int, required=True, choices=[6, 21], help='Chromosome number')
-    parser.add_argument('--model', type=str, required=True, choices=['SVC', 'WLR', 'XGB'], help='Model type')
+    parser.add_argument('--chr', type=int, required=True, choices=[6], help='Chromosome number')
+    parser.add_argument('--model', type=str, required=True, choices=['XGB'], help='Model type')
     args = parser.parse_args()
     main_refined_windows(args.chr, args.model)
